42775e4607090183191b8dc63926a052939eb790
DUCANH <zjnkill18@gmail.com>
2023-10-12 19:09:49 +0700

    update layout and font

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <div className="h1-bold">Nextjs 13</div>
-      <div className="h2-bold">Nextjs 13</div>
-      <div className="h3-bold">Nextjs 13</div>
-    </>
-  );
-}
